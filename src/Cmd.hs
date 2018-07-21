f0509228213a1d871731d6f064563ede96fa4a7c
Steve Purcell <steve@sanityinc.com>
2018-07-21 17:34:54 +1200

    Add strictness annotations

--- src/Cmd.hs
+++ src/Cmd.hs
@@ -11,26 +11,37 @@ data Cmd
   = Halt
   | Wait
   | FlipHarmonics
-  | SMove LLD
-  | LMove SLD
-          SLD
-  | Fission NCD
-            SeedAmount
-  | FusionP NCD
-  | FusionS NCD
-  | Fill NCD deriving (Show, Eq, Ord)
+  | SMove !LLD
+  | LMove !SLD
+          !SLD
+  | Fission !NCD
+            !SeedAmount
+  | FusionP !NCD
+  | FusionS !NCD
+  | Fill !NCD
+  deriving (Show, Eq, Ord)
 
-newtype SeedAmount = SeedAmount Int deriving (Show, Eq, Ord)
+newtype SeedAmount =
+  SeedAmount Int
+  deriving (Show, Eq, Ord)
 
 -- LLD: Long Linear Coordinate Difference
 -- SLD: Short Linear Coordinate Difference
 -- NCD: Near Coordinate Difference
 data VectorDiff = VectorDiff
-  { dx :: Int
-  , dy :: Int
-  , dz :: Int
+  { dx :: !Int
+  , dy :: !Int
+  , dz :: !Int
   } deriving (Eq, Ord, Show)
 
-newtype LLD = LLD VectorDiff deriving (Show, Eq, Ord)
-newtype SLD = SLD VectorDiff deriving (Show, Eq, Ord)
-newtype NCD = NCD VectorDiff deriving (Show, Eq, Ord)
+newtype LLD =
+  LLD VectorDiff
+  deriving (Show, Eq, Ord)
+
+newtype SLD =
+  SLD VectorDiff
+  deriving (Show, Eq, Ord)
+
+newtype NCD =
+  NCD VectorDiff
+  deriving (Show, Eq, Ord)
