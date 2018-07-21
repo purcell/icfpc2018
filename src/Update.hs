3eda01b49b7b3ced781a59dd04d2c7f928fbc79e
Kieran Trezona-le Comte <trezona.lecomte@gmail.com>
2018-07-21 14:44:21 +1200

    Push validation of Cmd validity into the same place as we perform Cmds
    Previously we thought it would be nice to validate Cmds at the site of
    generation, and only pass valid Cmds to be updated. Steve pointed out
    that this would result in duplicated work, as we'd effectively perform
    updates twice (once to check the validity of the resulting state, and a
    second time to actually perform the Cmd).
    
    This is lame, so instead we only return a new State if the Cmd was
    successsfully applied.
    
    Note that we're throwing away errors as far as the program is concerned
    - the cause of any invalid Cmds will be logged instead. This might get
    noisy, but we'll deal with that if it comes up.

--- src/Update.hs
+++ src/Update.hs
@@ -3,42 +3,41 @@
 
 module Update where
 
-import           Cmd      (Cmd (..), NCD (..))
+import           Cmd         (Cmd (..), NCD (..))
 import qualified Cmd
-import qualified Data.Set as Set
-import           Model    (Coordinate (..))
-import           State    (Bot, BotId, Energy, Harmonics (..), State (..),
-                           coord)
+import qualified Data.Set    as Set
+import           Debug.Trace as Debug
+import           Model       (Coordinate (..))
+import           State       (Bot, BotId, Energy, Harmonics (..), State (..),
+                              coord)
 import qualified State
 
-update :: State -> State
-update state@State{..} =
-  let commandsToPerform = take (length bots) trace
-  in
-    foldr performCommand state $ zip bots commandsToPerform
-
-
-performCommand :: (Bot, Cmd) -> State -> State
+performCommand :: (Bot, Cmd) -> State -> Maybe State
 performCommand (bot, cmd) state@State{energy, filledVoxels} =
-  case cmd of
-    Halt                   -> undefined
-    Wait                   -> state
-    FlipHarmonics          -> state { harmonics = flipHarmonics $ harmonics state }
-    SMove lld              -> undefined
-    LMove sld1 sld2        -> undefined
-    Fission ncd seedAmount -> undefined
-    FusionP ncd            -> undefined
-    FusionS ncd            -> undefined
-    Fill ncd               ->
-      state { filledVoxels = Set.insert coordFromNCD filledVoxels
-            , energy = energy + energyToFillVoxel
-            }
-      where coordFromNCD = addNCD ncd $ coord bot
-            energyToFillVoxel =
-              if Set.member coordFromNCD filledVoxels then
-                6
-              else
-                12
+  case State.checkForm newState of
+    Left err -> Debug.trace err Nothing
+    Right s  -> Just s
+  where
+    newState =
+      case cmd of
+        Halt                   -> undefined
+        Wait                   -> state
+        FlipHarmonics          -> state { harmonics = flipHarmonics $ harmonics state }
+        SMove lld              -> undefined
+        LMove sld1 sld2        -> undefined
+        Fission ncd seedAmount -> undefined
+        FusionP ncd            -> undefined
+        FusionS ncd            -> undefined
+        Fill ncd               ->
+          state { filledVoxels = Set.insert coordFromNCD filledVoxels
+                , energy = energy + energyToFillVoxel
+                }
+          where coordFromNCD = addNCD ncd $ coord bot
+                energyToFillVoxel =
+                  if Set.member coordFromNCD filledVoxels then
+                    6
+                  else
+                    12
 
 addNCD :: NCD -> Coordinate -> Coordinate
 addNCD (NCD (dx, dy, dz)) Coordinate{..} =
