56cddc96d10d3abdbd9489ccb6fc69b201a53116
Steve Purcell <steve@sanityinc.com>
2018-07-21 21:36:52 +1200

    Make the executable do something useful

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main where
 
+import qualified Data.ByteString.Lazy as BSL
+import Data.Semigroup ((<>))
+import Model
 import Solver
+import State
+import System.Environment (getArgs)
+import System.Exit (die)
+import Trace
 
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  case args of
+    [problemFile, traceFile] -> solveAndTrace problemFile traceFile
+    _ -> die "Usage: thisprog problemfile tracefile"
+
+solveAndTrace :: FilePath -> FilePath -> IO ()
+solveAndTrace problemFile traceFile = do
+  model <- modelFromFile problemFile
+  case solve model of
+    Just (endState, cost) -> do
+      putStrLn $ "Found solution which cost " <> show cost
+      BSL.writeFile traceFile (toBinaryTrace (trace endState))
+    _ -> die "No solution found"
