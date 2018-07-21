780ede88cd3f17bbe84168c0f1cb554f96425d7c
Steve Purcell <steve@sanityinc.com>
2018-07-21 18:39:01 +1200

    Add a very naïve astar solver

---
+++ src/Solver.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Solver where
+
+import Astar
+import Cmd
+import qualified Data.Map as Map
+import Data.Maybe (listToMaybe)
+import qualified Data.Set as Set
+import Model
+import State
+import Update
+
+solve :: Matrix -> Maybe (State, Int)
+solve m = listToMaybe $ astar (nexts m) (initialState m)
+
+nexts :: Matrix -> State -> [(State, Int, Int)]
+nexts target state =
+  [ (state, cost, distanceFromCompletion target state)
+  | nextState <- movesFromState state
+  , let cost = fromIntegral (energy nextState - energy state)
+  ]
+
+movesFromState :: State -> [State]
+movesFromState s =
+  [s' | botCmd <- possibleCommands s, Just s' <- [performCommand botCmd s]]
+
+possibleCommands :: State -> [(BotId, Cmd)]
+possibleCommands s =
+  [ (botId, cmd)
+  | (botId, bot) <- Map.toList (bots s)
+  , cmd <- commandsForBot s bot
+  ]
+
+commandsForBot :: State -> Bot -> [Cmd]
+commandsForBot _state _bot = [Halt] ++ smoves ++ lmoves ++ fills
+  where
+    deltas = [-15 .. (-1)] ++ [1 .. 15]
+    fills = Fill <$> nearCoordinateDiffs
+    smoves =
+      SMove . LLD <$>
+      ([VectorDiff d 0 0 | d <- deltas] ++
+       [VectorDiff 0 d 0 | d <- deltas] ++ [VectorDiff 0 0 d | d <- deltas])
+    lmoves = []
+
+nearCoordinateDiffs :: [NCD]
+nearCoordinateDiffs =
+  NCD <$>
+  [ diff
+  | dx <- [-1 .. 1]
+  , dy <- [-1 .. 1]
+  , dz <- [-1 .. 1]
+  , let diff = VectorDiff dx dy dz
+  , isNCD diff
+  ]
+    -- TODO: should be a smart constructor somewhere
+  where
+    isNCD d = manhattanDistance d <= 2 && chessboardLength d == 1
+
+distanceFromCompletion :: Matrix -> State -> Int
+distanceFromCompletion target s =
+  unfilledVoxelCount + remainingBots + harmonicSettingDistance
+  where
+    unfilledVoxelCount =
+      Set.size $
+      matrixFilledVoxels target `Set.difference` matrixFilledVoxels (matrix s)
+    remainingBots = length (bots s)
+    harmonicSettingDistance =
+      case harmonics s of
+        Low -> 0
+        High -> 1
